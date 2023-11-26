807bed5060c70cc32b818238e906c862bb484189
James Chen <jwc221@ic.ac.uk>
2023-11-26 15:06:03 +0000

    basic sampling tools

--- src/sampling.jl
+++ src/sampling.jl
@@ -1,9 +1,14 @@
-export sample, gue, haar
+export sample, gue, haar, diag
+
+using StatsBase, SpecialFunctions
+import StatsBase: sample
+import LinearAlgebra: diag
 
 function sample(m::AbstractJacobiMeasure{T}, n; c::Real=0) where T<:Real
     ret = Vector{T}(undef, n)
+    r = m_op(m) * m.ψ_k
     if c == 0
-        c = maximum(m[m.a:0.01:m.b])
+        c = maximum(r[m.a:0.01:m.b])
     end
     for i=1:n
         ret[i] = sample(m; c)
@@ -11,18 +16,61 @@ function sample(m::AbstractJacobiMeasure{T}, n; c::Real=0) where T<:Real
     ret
 end
 
-function sample(m::AbstractJacobiMeasure{T}; c::Real=0) where T<:Real
-    x = (m.b - m.a) * rand() + m.a
+function sample(m::JacobiMeasure{T}; c::Real=0) where T<:Real
+    r = jacobi(m.α, m.β, m.a .. m.b) * m.ψ_k
     if c == 0
-        c = maximum(m[m.a:0.01:m.b])
+        c = maximum(r[m.a:0.01:m.b])
     end
-    y = c*rand()
-    while m[x] < y
-        x = (m.b - m.a) * rand() + m.a
-        y = c*rand()
+    y = (m.b - m.a) * beta_inc_inv.(1+m.β, 1+m.α, rand())[1] + m.a
+    u = rand()
+    while u > r[y]/c
+        y = (m.b - m.a) * beta_inc_inv.(1+m.β, 1+m.α, rand())[1] + m.a
+        u = rand()
     end
-    x
+    y
 end
+
+function sample(m::ChebyshevUMeasure{T}; c::Real=0) where T<:Real
+    r = chebyshevu(m.a .. m.b) * m.ψ_k
+    if c == 0
+        c = maximum(r[m.a:0.01:m.b])
+    end
+    y = (m.b - m.a) * beta_inc_inv.(1.5, 1.5, rand())[1] + m.a
+    u = rand()
+    while u > r[y]/c
+        y = (m.b - m.a) * beta_inc_inv.(1.5, 1.5, rand())[1] + m.a
+        u = rand()
+    end
+    y
+end
+
+
+function sample(m::PointMeasure)
+    StatsBase.sample(m.λ, Weights(m.a))
+end
+
+function sample(m::PointMeasure, n=1)
+    StatsBase.sample(m.λ, Weights(m.a), n)
+end
+
+function sample(m::SumMeasure{T})  where T<:Real
+    s = length(m.m_k)
+    v = StatsBase.sample(1:s, Weights(sum.(m.m_k)))
+    sample(m.m_k[v])
+end
+
+function sample(m::SumMeasure{T}, n=1) where T<:Real
+    s = length(m.m_k)
+    v = StatsBase.sample(1:s, Weights(sum.(m.m_k)), n)
+    ret = Vector{T}(undef, n)
+    for i=1:s
+        w = findall(==(i), v)
+        ret[w] = sample(m.m_k[i], length(w))
+    end
+    ret
+end
+
+# demonstration functions
 
 function gue(n)
     A = randn(ComplexF64, n, n)
@@ -36,7 +84,13 @@ function haar(n)
     Q*s
 end
 
-
-function sample(m::PointMeasure, n=1)
-    StatsBasesample(m.λ, Weights(m.a))
+function diag(m, n)
+    Diagonal(sample(m, n))
 end
+
+n=1000
+Q = haar(n)
+A = diag(u, n)
+B = diag(u, n)
+C = Q*A*Q' + B
+histogram(real.(eigvals(C)), normalize=true, bins=50)
